329be6bd54831d194ba7cbc6ab42661ea3020d9a
yannhowe <yannhowe@gmail.com>
2020-04-01 15:04:05 +0800

    Only add buckets when necessary

--- chopchop/script.sh
+++ chopchop/script.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 mc config host add minio_host $MINIO_HOST $MINIO_ACCESS_KEY $MINIO_SECRET_KEY --api S3v4
-mc mb minio_host/$MINIO_TX_IN_BUCKET_NAME
-mc mb minio_host/$MINIO_TX_OUT_BUCKET_NAME
-mc mb minio_host/$MINIO_RX_IN_BUCKET_NAME
-mc mb minio_host/$MINIO_RX_OUT_BUCKET_NAME
+
 if [ "$TX" = "TRUE" ]; then
+    mc mb minio_host/$MINIO_TX_IN_BUCKET_NAME
+    mc mb minio_host/$MINIO_TX_OUT_BUCKET_NAME
     mc find minio_host/$MINIO_TX_IN_BUCKET_NAME --name '*' --watch | xargs -i /app/split.sh "{}"
 elif [ "$RX" = "TRUE" ]; then
+    mc mb minio_host/$MINIO_RX_IN_BUCKET_NAME
+    mc mb minio_host/$MINIO_RX_OUT_BUCKET_NAME
     mc find minio_host/$MINIO_RX_IN_BUCKET_NAME --name '*' --watch | xargs -i /app/join.sh "{}"
 fi
